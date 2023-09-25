993ba1e36423f79b6045a8715649c2faa6217333
Md-TarikulIslam <nayon1820@gmail.com>
2023-09-25 12:39:49 +0600

    25th Sep, 2023

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,13 +4,13 @@ import { FaFacebookF, FaGithub, FaGithubAlt, FaLinkedinIn, FaTwitter } from 'rea
 const Footer = () => {
     return (
         <div className='mx-auto py-10 bg-white bg-opacity-60 mt-10' style={{boxShadow:"1px 0px 7px #cad4de"}}>
-            <div>
+            <div className='mx-auto flex flex-col'>
                 <p className='text-center font-bold text-lg lg:text-2xl font'>Md. Tarikul Islam</p>
-                <div className='flex w-96 justify-center mt-5 mx-auto gap-2'>
-                    <a target='_blank' href="https://www.facebook.com/alwaysnayon"><FaFacebookF className='text-4xl rounded-xl hover:shadow-white shadow-sm bg-white p-2 transition   ease-in-out' /></a>
-                    <a target='_blank' href="https://www.linkedin.com/in/md-tarikul-islam-871134289/"> <FaLinkedinIn className='text-4xl rounded-xl hover:shadow-white shadow-sm bg-white p-2 transition   ease-in-out' /></a>
-                    <a target='_blank' href="https://twitter.com/tarikul_nayon"><FaTwitter className='text-4xl rounded-xl hover:shadow-white shadow-sm bg-white p-2 transition   ease-in-out' /></a>
-                    <a target='_blank' href="https://github.com/Md-TarikulIslam"> <FaGithubAlt className='text-4xl rounded-xl hover:shadow-white shadow-sm bg-white p-2 transition   ease-in-out' /></a>
+                <div className='flex w-96 justify-center mt-2 lg:mt-5 mx-auto gap-1 lg:gap-2 pb-8 lg:pb-0'>
+                    <a target='_blank' href="https://www.facebook.com/alwaysnayon"><FaFacebookF className='text-3xl rounded-lg  lg:text-4xl hover:shadow-white shadow-sm bg-white p-2 transition   ease-in-out' /></a>
+                    <a target='_blank' href="https://www.linkedin.com/in/md-tarikul-islam-871134289/"> <FaLinkedinIn className='text-3xl lg:text-4xl rounded-lg hover:shadow-white shadow-sm bg-white p-2 transition   ease-in-out' /></a>
+                    <a target='_blank' href="https://twitter.com/tarikul_nayon"><FaTwitter className='text-3xl rounded-lg lg:text-4xl hover:shadow-white shadow-sm bg-white p-2 transition   ease-in-out' /></a>
+                    <a target='_blank' href="https://github.com/Md-TarikulIslam"> <FaGithubAlt className='text-3xl lg:text-4xl rounded-lg hover:shadow-white shadow-sm bg-white p-2 transition   ease-in-out' /></a>
                 </div>
             </div>
         </div>
